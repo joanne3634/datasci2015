744940c94bd05a33d1dcd0c672ed13be8a30461a
Joanne <joanne3634@gmail.com>
2016-05-12 11:16:58 +0800

    fix bug
    - 微調 首頁 贊助人 樣式
    - 之前 traffic 圖更新成功
    - 404 的 ga

--- 404.php
+++ 404.php
@@ -130,7 +130,23 @@
     <!-- superfish menu  -->
     <script src="http://<?php echo  $_SERVER['HTTP_HOST']; ?>/js/vendor/jquery.hoverIntent.js"></script>
     <script src="http://<?php echo  $_SERVER['HTTP_HOST']; ?>/js/vendor/superfish.js"></script>
-        <script>
+    <script>
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-50828306-2', 'auto');
+    ga('send', 'pageview');
+    </script>
+    <script>
     $(function() {
         var includes = $('.include');
         jQuery.each(includes, function() {
